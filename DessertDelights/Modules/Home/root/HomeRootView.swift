a9bf5e1d832f047e235b243c19287af3aaaf551a
Mostafa10399 <mostafaelshall972@gmail.com>
2024-03-23 04:31:01 +0200

    create home root view and the view model

---
+++ DessertDelights/Modules/Home/root/HomeRootView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeRootView.swift
+//  DessertDelights
+//
+//  Created by Mostafa Mahmoud on 23/03/2024.
+//
+
+import SwiftUI
+
+struct HomeRootView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeRootView()
+}
---
+++ DessertDelights/Modules/Home/root/HomeRootViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeRootViewModel.swift
+//  DessertDelights
+//
+//  Created by Mostafa Mahmoud on 23/03/2024.
+//
+
+import Foundation
