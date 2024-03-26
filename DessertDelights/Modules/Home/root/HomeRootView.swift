9886cdcae7fb56c60992b1242d7213a5358736c2
Mostafa10399 <mostafaelshall972@gmail.com>
2024-03-26 19:27:03 +0200

    error handle

--- DessertDelights/Modules/Home/root/HomeRootView.swift
+++ DessertDelights/Modules/Home/root/HomeRootView.swift
@@ -42,6 +42,9 @@ struct HomeRootView: View {
             }
         }
         .displayLoader(isLoading: viewModel.isDataLoading)
+        .displayError(errorMessage: viewModel.errorMessage) {
+            viewModel.didTapOnTryAgain()
+        }
     }
 }
 
